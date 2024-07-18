d43fb1c23c497414dee880093a8c35d091315925
menezesja <santmjad@gmail.com>
2024-07-18 07:02:47 -0400

    configurações iniciais uipath

--- BotConexãoLinkedIn/build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "BotConexãoLinkedIn.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "BotConexãoLinkedIn.zip" -Force
--- BotConexãoLinkedIn/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "BotConexãoLinkedIn.zip" * -x "BotConexãoLinkedIn.zip"
